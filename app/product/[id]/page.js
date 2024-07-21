1a77646255ed8bc2016fdf92cf2c6d352c9475c5
Claire Yurev <claireyureva@gmail.com>
2024-07-21 02:54:18 -0700

    Update 2

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -60,7 +60,7 @@ export default function Product({ params }) {
                     Price: 
                     {product?.price 
                       ? <div className="font-bold text-[20px] ml-2">
-                          ///* USED TO BE: CAD ${(product?.price / 100).toFixed(2)} */
+                          {/* USED TO BE: CAD ${(product?.price / 100).toFixed(2)} */}
                           4{(product?.price / 100).toFixed(2)}
                         </div> 
                     : null }
